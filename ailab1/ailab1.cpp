832d0a5f10a7cbd6ef8eb554f40b702541840d5e
Kadetys <ku4in2005@gmail.com>
2024-09-08 14:38:32 +0300

    Выполнено задание 1

---
+++ ailab1/ailab1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+int main()
+{
+
+    int i = 0;
+    int m[5];
+    while (i < 5) {
+        m[i] = i + 1 * i;
+        i++;
+        }
+    for (int j = 0; j < 5; j++) {
+        printf("%d \n", m[j]);
+    }
+}
+
+
