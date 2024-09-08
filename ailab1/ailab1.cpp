7fa91974964fd029245eebaa600ddaaac29165fd
Kadetys <ku4in2005@gmail.com>
2024-09-08 14:42:14 +0300

    Выполнено задание 2

--- ailab1/ailab1.cpp
+++ ailab1/ailab1.cpp
@@ -1,15 +1,17 @@
 ﻿
 #include <iostream>
+#include <time.h>
 
 int main()
 {
 
     int i = 0;
     int m[5];
+    srand(time(0));
     while (i < 5) {
-        m[i] = i + 1 * i;
+        m[i] = rand() % 100;
         i++;
-        }
+    }
     for (int j = 0; j < 5; j++) {
         printf("%d \n", m[j]);
     }
