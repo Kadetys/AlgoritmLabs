c803b0f0fd97c45ffd2b5c0239d7d5f3b267b08e
Kadetys <ku4in2005@gmail.com>
2024-09-14 13:44:30 +0300

    Выполнено задание 1 работы 2

--- ailab1/ailab1.cpp
+++ ailab1/ailab1.cpp
@@ -1,111 +1,70 @@
-﻿
+﻿#include <time.h>
 #include <iostream>
-#include<locale.h>  
+#include<locale.h>
 
-struct students {
-    char family[20];
-    char name[20];
-    char faculty[20];
-    int number;
-}stud[3];
-#pragma warning(disable: 4996)
-
-int main()
+int main(void)
 {
-    setlocale(LC_ALL, "rus");
-    struct students s1;
-    int count;
-    int search;
-    char search_char[20];
-    int search_number;
-    int k = 0;
-    printf("Введите количество студентов\n");
-    scanf_s("%d", &count);
-    for (int i = 0; i < count; i++) {
-        printf("Введите фамилию студента %d\n", i + 1);
-        scanf("%s", stud[i].family);
-        rewind(stdin);
-        printf("Введите имя студента %d\n", i + 1);
-        scanf("%s", stud[i].name);
-        rewind(stdin);
-        printf("Введите факультет студента %d\n", i + 1);
-        scanf("%s", stud[i].faculty);
-        rewind(stdin);
-        printf("Введите номер зачётки студента %d\n", i + 1);
-        scanf("%d", &stud[i].number);
-        rewind(stdin);
-    }
-    while (1) {
-        printf("Выберите тип данных, по которому хотите найти студента:\n 1: Фамилия\n 2: Имя\n 3: Факультет\n 4: Номер зачетной книжки\n");
-        scanf("%d", &search);
-        switch (search) {
-        case 1:
-            printf("Введите фамилию студента\n");
-            scanf("%20s", search_char);
-            printf("Студенты, подходящте под заданные данные:\n");
-            for (int i = 0; i < count; i++) {
-                if (strcmp(stud[i].family, search_char) == 0) {
-                    printf("%s %s %s %d\n", stud[i].family, stud[i].name, stud[i].faculty, stud[i].number);
-                    k++;
-                }
-            }
-            if (k == 0) {
-                printf("Ни одного студента не найдено\n");
-            }
-            break;
-        case 2:
-            printf("Введите имя студента\n");
-            scanf("%20s", search_char);
-            printf("Студенты, подходящте под заданные данные:\n");
-            for (int i = 0; i < count; i++) {
-                if (strcmp(stud[i].name, search_char) == 0) {
-                    printf("%s %s %s %d\n", stud[i].family, stud[i].name, stud[i].faculty, stud[i].number);
-                    k++;
-                }
-            }
-            if (k == 0) {
-                ("Ни одного студента не найдено\n");
-            }
+	setlocale(LC_ALL, "");
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
 
-            break;
-        case 3:
-            printf("Введите факультет студента\n");
-            scanf("%20s", search_char);
-            printf("Студенты, подходящте под заданные данные:\n");
-            for (int i = 0; i < count; i++) {
-                if (strcmp(stud[i].faculty, search_char) == 0) {
-                    printf("%s %s %s %d\n", stud[i].family, stud[i].name, stud[i].faculty, stud[i].number);
-                    k++;
-                }
-            }
-            if (k == 0) {
-                printf("Ни одного студента не найдено\n");
-            }
-            break;
-        case 4:
-            printf("Введите номер зачетки студента\n");
-            scanf("%d", &search_number);
-            printf("Студенты, подходящте под заданные данные:\n");
-            for (int i = 0; i < count; i++) {
-                if (stud[i].number == search_number) {
-                    printf(" %s %s %s %d\n", stud[i].family, stud[i].name, stud[i].faculty, stud[i].number);
-                    k++;
-                }
-            }
-            if (k == 0) {
-                printf("Ни одного студента не найдено\n");
-            }
-            break;
+	clock_t start, end; // объявляем переменные для определения времени выполнения
 
-        default:
-            printf("Ошибка, выберите один из представленых пунктов");
-            break;
-        }
-        if (search > 0 and search < 5) {
-            break;
-        }
-    }
-            return 0;
-            }
+	int i = 0, j = 0, r;
+	int** a;
+	int** b;
+	int** c;
+	int elem_c = 0;
+	int m;
+	printf("Введите размер квадратной матрицы \n");
+	scanf_s("%d", &m);
+	a = (int**)malloc(m * sizeof(int*));
+	b = (int**)malloc(m * sizeof(int*));
+	c = (int**)malloc(m * sizeof(int*));
 
-        
+	for (i; i < m; i++) {
+		a[i] = (int*)malloc(m * sizeof(int));
+		b[i] = (int*)malloc(m * sizeof(int));
+		c[i] = (int*)malloc(m * sizeof(int));
+	}
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	while (i < m)
+	{
+		while (j < m)
+		{
+			a[i][j] = rand() % 100; // заполняем массив случайными числами
+			j++;
+		}
+		i++;
+	}
+	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	i = 0; j = 0;
+	while (i < m)
+	{
+		while (j < m)
+		{
+			b[i][j] = rand() % 100; // заполняем массив случайными числами
+			j++;
+		}
+		i++;
+	}
+	start = clock();
+
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			elem_c = 0;
+			for (r = 0; r < m; r++)
+			{
+				elem_c = elem_c + a[i][r] * b[r][j];
+				c[i][j] = elem_c;
+			}
+		}
+
+	}
+	end = clock();
+	printf("%f\n", float(end - start) / float(CLOCKS_PER_SEC));
+
+	return(0);
+}
