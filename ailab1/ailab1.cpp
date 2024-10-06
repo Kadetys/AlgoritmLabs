f6b9c8be4d9a1cf4356312e0e6b4b6a96aa55545
Kadetys <ku4in2005@gmail.com>
2024-10-06 14:12:02 +0300

    Выполнено задание 1

--- ailab1/ailab1.cpp
+++ ailab1/ailab1.cpp
@@ -3,205 +3,117 @@
 #include<string.h>
 #include<locale.h>
 #include<stdio.h>
-struct node
-{
-	char inf[256];  // полезная информация
-	int prioryty;
-	struct node* next; // ссылка на следующий элемент 
+struct Node {
+	int data;
+	struct Node* left;
+	struct Node* right;
 };
 
-//Обращение к списку и его элементам осуществляется посредством указателей :
+struct Node* root;
 
-struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
-int dlinna = 0;
-
-//Для списка реализованы функции создания, добавления, удаления элемента, просмотра списка, нахождения нужного элемента списка :
-
-// Функции добавления элемента, просмотра списка
-void spstore(void), review(void), del(char* name);
-
-char find_el[256];
-struct node* find(char* name); // функция нахождения элемента
-struct node* get_struct(void); // функция создания элемента
-
-
-
-struct node* get_struct(void)
+struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 {
-	struct node* p = NULL;
-	char s[256];
-	int prior = 0;
-
-	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+	if (r == NULL)
 	{
-		printf("Ошибка при распределении памяти\n");
-		exit(1);
-	}
-
-	printf("Введите название объекта: \n");   // вводим данные
-	scanf("%s", s);
-	if (*s == 0)
-	{
-		printf("Запись не была произведена\n");
-		return NULL;
-	}
-	printf("Укажите приоритет:\n");
-	scanf("%d", &prior);
-	strcpy(p->inf, s);
-	p -> prioryty = prior;
-	p -> next = NULL;
-
-	return p;		// возвращаем указатель на созданный элемент
-}
-
-/* Последовательное добавление в список элемента (в конец)*/
-void spstore(void)
-{
-	struct node* p = NULL;
-	p = get_struct();
-	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
-	{
-		head = p;
-		last = p;
-	}
-	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
-	{
-		last->next = p;
-		last = p;
-	}
-	return;
-}
-
-
-/* Просмотр содержимого списка. */
-void review(void)
-{
-	struct node* struc = head;
-	if (head == NULL)
-	{
-		printf("Список пуст\n");
-	}
-	while (struc)
-	{
-		printf("Имя - %s, \n", struc->inf);
-		struc = struc->next;
-	}
-	return;
-}
-
-
-
-
-/* Поиск элемента по содержимому. */
-struct node* find(char* name)
-{
-	struct node* struc = head;
-	if (head == NULL)
-	{
-		printf("Список пуст\n");
-	}
-	while (struc)
-	{
-		if (strcmp(name, struc->inf) == 0)
+		r = (struct Node*)malloc(sizeof(struct Node));
+		if (r == NULL)
 		{
-			
-			return struc;
+			printf("Ошибка выделения памяти");
+			exit(0);
 		}
-		struc = struc->next;
 
+		r->left = NULL;
+		r->right = NULL;
+		r->data = data;
+		if (root == NULL) return r;
+
+		if (data > root->data)	root->left = r;
+		else root->right = r;
+		return r;
 	}
-	printf("Элемент не найден\n");
-	return NULL;
 
+	if (data > r->data)
+		CreateTree(r, r->left, data);
+	else
+		CreateTree(r, r->right, data);
+
+	return root;
 }
-/* Удаление элемента по содержимому. */
-void del(char* name) {
-	struct node* struc = head;
-	struct node* prev = NULL;
-	int found = 0;
 
-	if (head == NULL) {
-		printf("Список пуст\n");
+//Функция вывода дерева на экран(дерево выводится повёрнутым на 90 градусов, корень находится слева) :
+
+	void print_tree(struct Node* r, int l)
+{
+
+	if (r == NULL)
+	{
 		return;
 	}
 
-	// Проход по списку для поиска и удаления всех совпадающих элементов
-	while (struc) {
-		if (strcmp(name, struc->inf) == 0) {
-			found = 1;  // Элемент найден
-
-			// Удаление первого элемента
-			if (prev == NULL) {
-				head = struc->next;
-				free(struc);
-				struc = head;  // обновляем текущий узел после удаления головы
-			}
-			else {  // Удаление элемента не в начале списка
-				prev->next = struc->next;
-				free(struc);
-				struc = prev->next;  // обновляем текущий узел
-			}
-
-			printf("Элемент удален\n");
-		}
-		else {
-			prev = struc;  // Обновляем предыдущий элемент
-			struc = struc->next;  // Переходим к следующему элементу
-		}
+	print_tree(r->right, l + 1);
+	for (int i = 0; i < l; i++)
+	{
+		printf(" ");
 	}
 
-	if (!found) {
-		printf("Элемент не найден\n");
-	}
+	printf("%d\n", r->data);
+	print_tree(r->left, l + 1);
 }
 
-int main() {
-	setlocale(LC_ALL, "rus");
-	char s[10];
-	int c = 0;
-	struct node* vrem;
-	do {
-		printf("\nМеню:\n");
-		printf("1. Добавить элемент\n");
-		printf("2. Просмотреть список\n");
-		printf("3. Удалить элемент\n");
-		printf("4. Поиск элемента\n");
-		printf("5. Выход\n");
-		printf("Выберите один из пунктов: ");
-		scanf_s("%d", &c);
-
-		switch (c) {
-		case 1:
-			spstore();
-			break;
-		case 2:
-
-			review();
-			break;
-
-		case 3:
-			printf("Введите название объекта для удаления: ");
-			scanf_s("%s", s, (unsigned)_countof(s));
-			del(s);
-			break;
-		case 4:
-			printf("Введите элемент для поиска: ");
-			scanf_s("%s", s, (unsigned)_countof(s));
-			vrem = find(s);
-			if (vrem != NULL) {
-				printf("Элемент  найден!\n"); // Вывести данные вне функции, сохранить указатель, вывести номер в структуре
-				printf("Элемент: %s\n Приотритет элемента: %d", vrem->inf, vrem -> prioryty);
-			}
-			break;
-
-		case 5:
-			printf("Выход...\n");
-			break;
-		default:
-			printf("Ошибка. Попробуйте снова.\n");
-			break;
+	int find_tree(struct Node* r, int status, int* orig)
+	{
+		if (r == NULL or status == 1)
+		{
+			return 1;
 		}
-	} while (c != 5);
+		if (r->data == *orig) 
+		{
+			printf("Элемент найден\n");
+			status = 1;
+			return 1;
+		}
+		find_tree(r->right, status, orig);
+		find_tree(r->left, status, orig);
+		return 0;
+	}
+//Программа, использующая приведенные функции :
 
+int main()
+{
+	setlocale(LC_ALL, "");
+	int D, start = 1;
+
+	root = NULL;
+	printf("Для окончания заполнения дерева введите значение -1\n");
+	while (start)
+	{
+		printf("Введите число: ");
+		scanf("%d", &D);
+		if (D == -1)
+		{
+			printf("Построение дерева окончено\n\n");
+			start = 0;
+		}
+		else
+			root = CreateTree(root, root, D);
+
+	}
+	print_tree(root, 0);
+	int H, find = 1;
+	int itog = 0;
+	while (find) {
+		printf("Хотите начать поиск элемента?\n 0. Нет\n 1. Да\n");
+		scanf("%d", &find);
+		if (find == 1) {
+			printf("Введите элемент, который желаете найти\n");
+			scanf("%d", &H);
+			itog = find_tree(root, 0, &H);
+			if (itog != 1) {
+				printf("Элемент не найден\n");
+			}
+		}
+
+
+	}
 	return 0;
 }
